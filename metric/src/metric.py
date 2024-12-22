8465628276560202ccfe4701acb4799dfe66903d
Xjoraas74 <64475202+Xjoraas74@users.noreply.github.com>
2024-12-23 05:57:43 +0700

    Fix

--- metric/src/metric.py
+++ metric/src/metric.py
@@ -7,12 +7,12 @@ try:
     # Создаём подключение к серверу на локальном хосте
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
-   
+
     # Объявляем очередь y_true
     channel.queue_declare(queue='y_true')
     # Объявляем очередь y_pred
     channel.queue_declare(queue='y_pred')
- 
+
     # Создаём функцию callback для обработки данных из очереди
     def callback(ch, method, properties, body):
         message = json.loads(body)
@@ -21,23 +21,23 @@ try:
         answer_string = f'Из очереди {method.routing_key} получено значение {value}'
         with open('./logs/labels_log.txt', 'a') as log:
             log.write(answer_string +'\n')
-
-        data = pd.read_csv('./logs/metric_log.csv', index_col='id')
-        if message['id'] not in data.index:
+        
+        data = pd.read_csv('./logs/metric_log.csv')
+        if message['id'] not in data['id'].values:
             new_entry = pd.DataFrame(
                 data={
-                    'y_true': value if method.routing_key == 'y_true' else pd.NA,
-                    'y_pred': value if method.routing_key == 'y_pred' else pd.NA,
+                    'id': message['id'],
+                    'y_true': [value if method.routing_key == 'y_true' else pd.NA],
+                    'y_pred': [value if method.routing_key == 'y_pred' else pd.NA],
                     'absolute_error': pd.NA
                 },
-                index=message['id']
             )
-            data = pd.concat(data, new_entry)
+            data = pd.concat([data, new_entry])
         else:
-            data.loc[message['id']][method.routing_key] = value
-            ae = abs(data.loc[message['id']]['y_true'] - data.loc[message['id']]['y_pred'])
-            data.loc[message['id']]['absolute_error'] = ae
-        data.to_csv('./logs/metrics_log.csv')
+            data.loc[data['id'] == message['id'], method.routing_key] = value
+            ae = abs(data.loc[data['id'] == message['id'], 'y_true'] - data.loc[data['id'] == message['id'], 'y_pred'])
+            data.loc[data['id'] == message['id'], 'absolute_error'] = ae
+        data.to_csv('./logs/metric_log.csv', index=False)
 
     # Извлекаем сообщение из очереди y_true
     channel.basic_consume(
@@ -51,7 +51,7 @@ try:
         on_message_callback=callback,
         auto_ack=True
     )
- 
+
     # Запускаем режим ожидания прихода сообщений
     print('...Ожидание сообщений, для выхода нажмите CTRL+C')
     channel.start_consuming()
--- plot/src/plot.py
+++ plot/src/plot.py
@@ -6,9 +6,10 @@ import seaborn as sns
 
 while True:
     data = pd.read_csv('./logs/metric_log.csv', index_col='id')
-    sns.histplot(data['absolute_error'], kde=True)
+    sns.histplot(data['absolute_error'], stat='count', kde=True)
     plt.xlabel('absolute_error')
     plt.ylabel('Count')
     plt.savefig('./logs/error_distribution.png')
+    plt.close()
 
     time.sleep(10)
